a5e458b89fdf903629ed958c6eb3572638fcb026
MichalZalecki <zalecki.post@gmail.com>
2015-02-05 20:25:01 +0100

    firefox has been commented out due to angular/protractor#1734 issue

--- e2e-tests/conf.js
+++ e2e-tests/conf.js
@@ -2,8 +2,8 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['spec.coffee'],
   multiCapabilities: [{
-    'browserName': 'firefox'
-  }, {
-    'browserName': 'chrome'
-  }]
-}
+      'browserName': 'chrome'
+  }/*,{
+      'browserName': 'firefox'
+  }*/]
+};
